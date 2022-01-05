bd5572ac57300995a054e3629c493fe18889cfc3
Dmitry Lukashenko <d.a.lukashenko@gmail.com>
2022-01-05 09:28:04 +0200

    feedback app is ready to deploying

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import Card from './components/shared/Card'
+import FeedbackItem from './components/FeedbackItem'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './pages/AboutPage'
+import {FeedbackProvider} from './context/FeedbackContext'
+import AboutIconLink from './components/AboutIconLink'
+
+
 function App(){
-    return (
-        <div>
-            <h1>FeedBack App</h1>
-            <p>Hello! This is very cool feedback application</p>
-        </div>
-        
+
+    return ( 
+        <FeedbackProvider>    
+            <Router>     
+            <Header/>
+            <div className='container'>    
+                <Routes>        
+                <Route exact path='/' element={
+                    <>
+                    <FeedbackForm />
+                    <FeedbackStats />
+                    <FeedbackList />
+                    </>
+                }>                   
+                </Route>               
+                <Route path='/about' element={<AboutPage />}/>           
+                </Routes>
+                <AboutIconLink />
+            </div>        
+            </Router>
+        </FeedbackProvider>    
     )
 }
 
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,16 @@
+const FeedbackData = [
+//     {id:1,
+//     rating:10,
+//     text: 'Lorem ipsum dolor sit, amet consectetu adipisicing elit.', 
+//     },
+//     {id:2,
+//     rating:9,
+//     text: 'Lorem ipsum dolor sit, amet consectetu adipisicing elit.', 
+//     },
+//     {id:3,
+//     rating:8,
+//     text: 'Lorem ipsum dolor sit, amet consectetu adipisicing elit.', 
+//     }
+];
+
+export default FeedbackData;
